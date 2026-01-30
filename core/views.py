34ddd31ee200b0b0b1ee4e36f45fa01d0bb8f994
Meredith Roncjero <meredithroncejero09@gmail.com>
2026-01-30 16:40:51 +0800

    landing page

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to our E-Commerce Website")
+    return render(request, 'core/home.html')
